8143fd3e3d84530d946441be33d4229f06d855f1
Koraytopuz <91742967+Koraytopuz@users.noreply.github.com>
2023-03-10 21:25:22 +0300

    Add files via upload

---
+++ Klavyeden iki sayı alarak bu sayıların mantıksal karşılaştırmalarını yapan kod.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"lutfen birinci sayiyi giriniz"<< endl;
+	cin>> x; 
+	cout<<"lutfen ikinci sayiyi giriniz"<< endl;
+	cin>> y;
+	if(x==y)
+	{
+		cout<<"sayilar esittir"<< endl;
+		
+	}
+	else
+	{
+		cout<<"sayilar esit degildir"<< endl;
+	}
+	if(x!=y)
+	
+	cout<<"sayilar farklidir"<< endl;
+
+	else
+	
+		cout<<"sayilar farkli degildir"<< endl;
+			if(x<y)
+			
+				cout<<"ilk sayi kucuktur"<< endl;
+			
+			else
+			
+				cout<<"ilk sayi kucuk degildir"<< endl;
+			
+			if(x>y)
+			
+             cout<<"ikinci sayi kucuktur"<< endl;				
+			
+			else
+			
+				cout<<"ikinci sayi kucuk degildir"<< endl;
+			
+			if(x<=y)
+			
+				cout<<"ilk sayi kucuk esittir"<< endl;
+			
+			else
+			
+				cout<<"ilk sayi kucuk esit degildir"<< endl;
+			
+			if(x>=y)
+			
+				cout<<"ikinci sayi kucuk esittir"<< endl;
+			
+			else
+			
+				cout<<"ikinci sayi kucuk esit degildir"<< endl;
+			
+}
+
+
+
+
+
+
+
